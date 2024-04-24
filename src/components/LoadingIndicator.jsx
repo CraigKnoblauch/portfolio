dc7372164106a0a31a50496aeb30245874b232ec
Craig Knobaluch <craigknoblauch@gmail.com>
2024-04-23 22:26:20 -0400

    fragment color animating with progress uniform; now creating Source map error warnings

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -1,45 +1,39 @@
 import * as Three from 'three'
 import { shaderMaterial } from "@react-three/drei"
-import floorFragmentShader from '../shaders/loading-indicator/fragment.glsl'
-import floorVertexShader from '../shaders/loading-indicator/vertex.glsl'
+import loadingIndicatorFragmentShader from '../shaders/loading-indicator/fragment.glsl'
+import loadingIndicatorVertexShader from '../shaders/loading-indicator/vertex.glsl'
 import { useMemo, useRef } from 'react'
-import { useFrame } from '@react-three/fiber'
+import { useFrame, extend } from '@react-three/fiber'
+
+const LoadingIndicatorMaterial = shaderMaterial(
+    {
+        uProgress: 1.0
+    },
+    loadingIndicatorVertexShader,
+    loadingIndicatorFragmentShader
+)
+
+extend({ LoadingIndicatorMaterial })
 
 export default function LoadingIndicator() {
 
     const meshRef = useRef()
-    const shaderMaterialRef = useRef()
-
-    const uniforms = useMemo(
-        () => ({
-          uProgress: {
-            value: 1.0,
-          }
-        }), []
-    )
+    const loadingIndicatorMaterialRef = useRef()
 
     useFrame((state, delta) => {
         // Increment the progress of the loading indicator by 0.01 every frame
-        shaderMaterialRef.current.uProgress.value += 0.01
+        loadingIndicatorMaterialRef.current.uProgress += 0.01
 
         // When the progress reaches 1, it will be reset to 0
-        if (shaderMaterialRef.current.uProgress.value >= 1.0) {
-            shaderMaterialRef.current.uProgress.value = 0
+        if (loadingIndicatorMaterialRef.current.uProgress >= 1.0) {
+            loadingIndicatorMaterialRef.current.uProgress = 0
         }
-
-        console.log(shaderMaterialRef.current.uProgress.value)
     })
     
     return <>
         <mesh ref={meshRef} rotation={[-89.5, 0, 0]}>
             <planeGeometry args={[6, 6, 10, 10]} />
-            <shaderMaterial 
-                uniforms={uniforms}
-                fragmentShader={floorFragmentShader}
-                vertexShader={floorVertexShader}
-                transparent = { true }
-                ref={shaderMaterialRef}
-            />
+            <loadingIndicatorMaterial ref={loadingIndicatorMaterialRef} />
         </mesh>
     </>
 }
