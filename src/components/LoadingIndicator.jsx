135e72882565b7505850b37378aa5b0bc88d4efe
Craig Knobaluch <craigknoblauch@gmail.com>
2024-04-23 22:18:50 -0400

    working on getting progress uniform into fragment

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -1,28 +1,44 @@
 import * as Three from 'three'
-import { PivotControls, shaderMaterial } from "@react-three/drei"
+import { shaderMaterial } from "@react-three/drei"
 import floorFragmentShader from '../shaders/loading-indicator/fragment.glsl'
 import floorVertexShader from '../shaders/loading-indicator/vertex.glsl'
 import { useMemo, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
 
 export default function LoadingIndicator() {
 
     const meshRef = useRef()
+    const shaderMaterialRef = useRef()
 
     const uniforms = useMemo(
         () => ({
-          u_time: {
-            value: 0.0,
+          uProgress: {
+            value: 1.0,
           }
         }), []
     )
+
+    useFrame((state, delta) => {
+        // Increment the progress of the loading indicator by 0.01 every frame
+        shaderMaterialRef.current.uProgress.value += 0.01
+
+        // When the progress reaches 1, it will be reset to 0
+        if (shaderMaterialRef.current.uProgress.value >= 1.0) {
+            shaderMaterialRef.current.uProgress.value = 0
+        }
+
+        console.log(shaderMaterialRef.current.uProgress.value)
+    })
     
     return <>
         <mesh ref={meshRef} rotation={[-89.5, 0, 0]}>
-            <planeGeometry args={[5, 5, 10, 10]} />
+            <planeGeometry args={[6, 6, 10, 10]} />
             <shaderMaterial 
                 uniforms={uniforms}
                 fragmentShader={floorFragmentShader}
                 vertexShader={floorVertexShader}
+                transparent = { true }
+                ref={shaderMaterialRef}
             />
         </mesh>
     </>
--- src/shaders/loading-indicator/fragment.glsl
+++ src/shaders/loading-indicator/fragment.glsl
@@ -2,11 +2,20 @@
 
 varying vec2 vUv;
 
+uniform float uProgress;
+
 void main()
 {
-    float strength = step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
     
-    vec3 mixedColor = mix(vec3(0.0), vec3(1.0), strength);
+    float strength = step(0.01, abs(distance(vUv, vec2(0.5)) - 0.25));
+
+    float angle = (atan(vUv.x, vUv.y) + PI) / (PI * 2.0);
+    if (angle < 0.5)
+    {
+        discard;
+    }
+
+    vec3 mixedColor = mix(vec3(0.0), vec3(uProgress, 1.0, 0), strength);
 
     gl_FragColor = vec4(mixedColor, 1.0);
 }
