74ee3234e32fed6626c8edbfad37d4797dd210ce
Craig Knobaluch <craigknoblauch@gmail.com>
2024-04-29 00:02:37 -0400

    with a combination of the new approach and the generated code, the animations finally work

--- src/components/Rabbit.jsx
+++ src/components/Rabbit.jsx
@@ -4,17 +4,20 @@ import { useRef, useEffect } from "react"
 import { useFrame } from "@react-three/fiber"
 import * as THREE from "three"
 
-export default function Rabbit() {
+export default function Rabbit(props) {
 
+    const group = useRef()
     const body = useRef()
     const model = useGLTF('./models/rabbit.glb')
-    const { actions, names } = useAnimations(model.animations, body)
+    const { actions, names } = useAnimations(model.animations, group)
     console.log("Actions: ", actions)
 
     // Keyboard controls
     const [ subscribeKeys, getKeys ] = useKeyboardControls()
     useFrame((state, delta) => {
         const keys = getKeys()
+
+        actions["walk/jump"].play()
 
         // Get current position and rotation
         const position = body.current.translation()
@@ -48,16 +51,36 @@ export default function Rabbit() {
     console.log(model)
 
     return <>
-        <RigidBody ref={body} 
-                   canSleep={false} 
-                   restitution={0.2} 
-                   friction={1} 
-                   linearDamping={0.5} // The damping allows the ball to come to a stop
-                   angularDamping={0.5}
-                   gravityScale={0}
-                   position={[0, 0, 0]}
-         >
-            <primitive object={model.scene} />
-        </RigidBody>
+        <group ref={group} {...props} dispose={null}>
+            <RigidBody ref={body} 
+                    canSleep={false} 
+                    restitution={0.2} 
+                    friction={1} 
+                    linearDamping={0.5} // The damping allows the ball to come to a stop
+                    angularDamping={0.5}
+                    gravityScale={0}
+                    position={[0, 0, 0]}
+            >
+                <group ref={group} name="Scene">
+                    <group name="metarig" rotation={[0.015, 0, 0]} scale={4.716}>
+                    <primitive object={model.nodes.pelvisC} />
+                    <primitive object={model.nodes.control_frontl} />
+                    <primitive object={model.nodes.pole_target_frontl} />
+                    <primitive object={model.nodes.control_frontr} />
+                    <primitive object={model.nodes.pole_target_frontr} />
+                    <primitive object={model.nodes.control_backl} />
+                    <primitive object={model.nodes.pole_backl} />
+                    <primitive object={model.nodes.control_backr} />
+                    <primitive object={model.nodes.pole_backr} />
+                    <group name="rabbit">
+                        <skinnedMesh name="Plane" geometry={model.nodes.Plane.geometry} material={model.materials.brown} skeleton={model.nodes.Plane.skeleton} />
+                        <skinnedMesh name="Plane_1" geometry={model.nodes.Plane_1.geometry} material={model.materials.white} skeleton={model.nodes.Plane_1.skeleton} />
+                        <skinnedMesh name="Plane_2" geometry={model.nodes.Plane_2.geometry} material={model.materials.black} skeleton={model.nodes.Plane_2.skeleton} />
+                    </group>
+                    </group>
+                </group>
+                
+            </RigidBody>
+        </group>
     </>
 }
