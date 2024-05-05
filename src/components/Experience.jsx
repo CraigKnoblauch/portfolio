41fe7631d0870258c648723968efcada5be695f8
Craig Knobaluch <craigknoblauch@gmail.com>
2024-05-05 00:08:32 -0400

    can now do absolute imports; will change imports slowly as I move on in the future

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,20 +1,20 @@
 import { OrbitControls } from "@react-three/drei"
-import Rabbit from "./Rabbit.jsx"
-import LoadingIndicator from "./LoadingIndicator.jsx"
+import Rabbit from "src/components/Rabbit.jsx"
+import LoadingIndicator from "src/components/LoadingIndicator.jsx"
 import { Physics } from "@react-three/rapier"
-import MovementDebugPlane from "./MovementDebugPlane.jsx"
-import Ground from "./Ground.jsx"
-import AsuCaption from "./captions/AsuCaption.jsx"
-import RabbitHoleArea from "./areas/RabbitHoleArea.jsx"
-import CareerArea from "./areas/CareerArea.jsx"
-import ProjectsArea from "./areas/ProjectsArea.jsx"
-import ContactArea from "./areas/ContactArea.jsx"
-import HobbiesArea from "./areas/HobbiesArea.jsx"
+import MovementDebugPlane from "src/components/MovementDebugPlane.jsx"
+import Ground from "src/components/Ground.jsx"
+import AsuCaption from "src/components/captions/AsuCaption.jsx"
+import RabbitHoleArea from "src/components/areas/RabbitHoleArea.jsx"
+import CareerArea from "src/components/areas/CareerArea.jsx"
+import ProjectsArea from "src/components/areas/ProjectsArea.jsx"
+import ContactArea from "src/components/areas/ContactArea.jsx"
+import HobbiesArea from "src/components/areas/HobbiesArea.jsx"
 
 const Experience = () => {
     return (
         <>
-            <Physics debug={false}>
+            <Physics debug={true}>
                 <ambientLight />
                 <OrbitControls />
                 <group>
@@ -23,7 +23,7 @@ const Experience = () => {
                     <HobbiesArea />
                     <ProjectsArea />
                     <ContactArea />
-                    <MovementDebugPlane />
+                    {/* <MovementDebugPlane */}
                     <Rabbit />
                 </group>
             </Physics>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import glsl from 'vite-plugin-glsl'
+import { Component } from 'react'
 
 // https://vitejs.dev/config/
 // Added glsl() on recomendation to resolve syntax error on glsl import: https://www.reddit.com/r/threejs/comments/10q4cnl/shader_file_throws_error_unexpected_token/?rdt=60672
 export default defineConfig({
   plugins: [react(), glsl()],
+  resolve: {
+    alias: {
+      src: "/src/",
+    }
+  }
 })
