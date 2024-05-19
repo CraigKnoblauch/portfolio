c2074b146d60dcef63bf0844fdaad407a97ee8b6
Craig Knobaluch <craigknoblauch@gmail.com>
2024-05-18 20:57:26 -0400

    launch button pressed by rabbit initiates launch animation; launch animation feels like it needs refactoring

--- src/components/RearwardExhaust.jsx
+++ src/components/RearwardExhaust.jsx
@@ -5,7 +5,7 @@ import * as THREE from 'three'
 import MatcapManager from 'src/MatcapManager.js'
 import FifoQueue from 'src/FifoQueue.js'
 
-export default function RearwardExhaust({emitter}) {
+export default function RearwardExhaust({emitter, isVisibleRef}) {
 
     if (!emitter) {
         throw new Error("Emitter for rearward exhaust is not defined.");
@@ -25,64 +25,66 @@ export default function RearwardExhaust({emitter}) {
 
     useFrame((state, delta) => {
         
-        const dodecahedron = new THREE.Mesh(
-            new THREE.DodecahedronGeometry(1),
-            new THREE.MeshMatcapMaterial({ matcap: materials[Math.floor(Math.random() * materials.length)] })
-        );
-        dodecahedron.position.copy(emitter.position);
+        if ( isVisibleRef.current ) { // NOTE TODO Using a ref like this is not the best way to handle this. It's a workaround for now.
+            const dodecahedron = new THREE.Mesh(
+                new THREE.DodecahedronGeometry(1),
+                new THREE.MeshMatcapMaterial({ matcap: materials[Math.floor(Math.random() * materials.length)] })
+            );
+            dodecahedron.position.copy(emitter.position);
 
-        // Initialize the velocity and spin of the exhaust element
-        dodecahedron.velocity = new THREE.Vector3(
-            1 * Math.sin(emitter.rotation.y + 10 + (0.25 * (Math.random()*2-1))), // Last bit adds a random deviation in the x and z directions
-            0.1,
-            -1 * Math.cos(emitter.rotation.y + 1 + (0.25 * (Math.random()*2-1)))
-        );
-        dodecahedron.spin = new THREE.Vector3(
-            Math.random() * 0.1,
-            Math.random() * 0.1,
-            Math.random() * 0.1
-        );
+            // Initialize the velocity and spin of the exhaust element
+            dodecahedron.velocity = new THREE.Vector3(
+                1 * Math.sin(emitter.rotation.y + 10 + (0.25 * (Math.random()*2-1))), // Last bit adds a random deviation in the x and z directions
+                0.1,
+                -1 * Math.cos(emitter.rotation.y + 1 + (0.25 * (Math.random()*2-1)))
+            );
+            dodecahedron.spin = new THREE.Vector3(
+                Math.random() * 0.1,
+                Math.random() * 0.1,
+                Math.random() * 0.1
+            );
 
-        // Add the new element to the group and the queue
-        // If an element was removed from the queue, remove it from the group
-        groupRef.current.add(dodecahedron);
-        const last = queue.enqueue(dodecahedron);
-        if (last) {
-            groupRef.current.remove(last);
+            // Add the new element to the group and the queue
+            // If an element was removed from the queue, remove it from the group
+            groupRef.current.add(dodecahedron);
+            const last = queue.enqueue(dodecahedron);
+            if (last) {
+                groupRef.current.remove(last);
+            }
+
+            for (const element of queue) {
+
+                // Calculate the distance to the emitter
+                const distance = element.position.distanceTo(emitter.position);
+
+                // Decrease the velocity as a function of the distance to the emitter
+                const distanceScale = Math.random() * (0.1 - 0.001) + 0.001
+                const decreaseFactor = Math.max(0, 1 - distance * .005);
+                
+                element.velocity.multiplyScalar(decreaseFactor);
+
+                // Move the element according to its velocity
+                element.position.add(element.velocity);
+
+                // Increase the y position as a function of the distance to the emitter
+                const increaseFactor = Math.max(0, distance * 0.0075);
+                element.position.y += increaseFactor;
+
+                // Adjust the scale of the element as a function of the distance to the emitter
+                const scaleFactor = Math.max(0.6, distance * 0.075);
+                element.scale.set(scaleFactor, scaleFactor, scaleFactor);
+
+                // Decrease the spin as a function of the distance to the emitter
+                const spinDecreaseFactor = Math.max(0, 1 - distance * 0.001);
+                element.spin.multiplyScalar(spinDecreaseFactor);
+
+                // Apply the spin to the element
+                element.rotation.x += element.spin.x;
+                element.rotation.y += element.spin.y;
+                element.rotation.z += element.spin.z;
+
+            };
         }
-
-        for (const element of queue) {
-
-            // Calculate the distance to the emitter
-            const distance = element.position.distanceTo(emitter.position);
-
-            // Decrease the velocity as a function of the distance to the emitter
-            const distanceScale = Math.random() * (0.1 - 0.001) + 0.001
-            const decreaseFactor = Math.max(0, 1 - distance * .005);
-            
-            element.velocity.multiplyScalar(decreaseFactor);
-
-            // Move the element according to its velocity
-            element.position.add(element.velocity);
-
-            // Increase the y position as a function of the distance to the emitter
-            const increaseFactor = Math.max(0, distance * 0.0075);
-            element.position.y += increaseFactor;
-
-            // Adjust the scale of the element as a function of the distance to the emitter
-            const scaleFactor = Math.max(0.6, distance * 0.075);
-            element.scale.set(scaleFactor, scaleFactor, scaleFactor);
-
-            // Decrease the spin as a function of the distance to the emitter
-            const spinDecreaseFactor = Math.max(0, 1 - distance * 0.001);
-            element.spin.multiplyScalar(spinDecreaseFactor);
-
-            // Apply the spin to the element
-            element.rotation.x += element.spin.x;
-            element.rotation.y += element.spin.y;
-            element.rotation.z += element.spin.z;
-
-        };
     })
 
     return <>
--- src/components/areas/CareerArea.jsx
+++ src/components/areas/CareerArea.jsx
@@ -7,6 +7,7 @@ import GenericArea from "src/components/areas/GenericArea"
 import RearwardExhaust from "src/components/RearwardExhaust.jsx"
 import RocketFlames from "src/components/RocketFlames.jsx"
 import Caption from "src/components/Caption.jsx"
+import { RigidBody } from "@react-three/rapier"
 
 export default function CareerArea(props) {
     const { nodes } = useGLTF('./models/career-area.glb')
@@ -27,28 +28,53 @@ export default function CareerArea(props) {
     const cradleRotationLimit = cradleRef.current.rotation.z - Math.PI/2
     let cradleRotationalVelocity = 0.01
 
+    const launchButtonRef = useRef()
+    const launchButtonZTarget = nodes.launch_button.position.z - 0.09
+    const launchState = {
+        initLaunch: false,
+        hasLaunched: false
+    }
+    const exhaustIsVisibleRef = useRef(false)
+
+    /**
+     * [x] Move the launch button into the launch button platform so it appears to be pressed
+     * [ ] Set hasLauched to true so lauch animation cannot repeat
+     * [ ] TODO Move camera to a rocket viewing position, then back to look at the rabbit afterwards
+     * [x] Initiate launch animation
+     */
     useFrame((state, delta) => {
-        // temporary for debugging
-        // state.camera.lookAt(nodes.rocket.position)
 
-        // ****************************************************
-        // Rocket Launch Animation
-        // ****************************************************
-        // Move the rocket upwards, slowly at first and then gaining speed
-        if (rocketGroupRef.current.position.y < 5) { // TODO remove this: Stop the rocket for debugging purposes
-            rocketGroupRef.current.position.y += initialRocketSpeed;
-            initialRocketSpeed += rocketAcceleration;
+        // Rocket launch animation if the user has requested it and it hasn't already happened
+        if (launchState.initLaunch && !launchState.hasLaunched) {
+            
+            // Move the launch button into the platform
+            if (launchButtonRef.current.position.z > launchButtonZTarget) {
+                launchButtonRef.current.position.z -= 0.005
+            }
+
+            // Show the exhaust fumes
+            exhaustIsVisibleRef.current = true
+
+            // ****************************************************
+            // Rocket Launch Animation
+            // ****************************************************
+            // Move the rocket upwards, slowly at first and then gaining speed
+            if (rocketGroupRef.current.position.y < 5) { // TODO remove this: Stop the rocket for debugging purposes
+                rocketGroupRef.current.position.y += initialRocketSpeed;
+                initialRocketSpeed += rocketAcceleration;
+            }
+
+            // Rotate the cradle until it falls to a set position
+            if (cradleRef.current.rotation.z > cradleRotationLimit) {
+                // cradleRef.current.rotation.z -= 0.01
+                cradleRef.current.rotation.z -= cradleRotationalVelocity * 0.1;
+                cradleRotationalVelocity += 0.05;
+            }
+
+            // end rocket launch animation
+            // ****************************************************
         }
-
-        // Rotate the cradle until it falls to a set position
-        if (cradleRef.current.rotation.z > cradleRotationLimit) {
-            // cradleRef.current.rotation.z -= 0.01
-            cradleRef.current.rotation.z -= cradleRotationalVelocity * 0.1;
-            cradleRotationalVelocity += 0.05;
-        }
-
-        // end rocket launch animation
-        // ****************************************************
+        // TODO Under what conditions can I say the launch has completed?
 
     });
     
@@ -57,7 +83,10 @@ export default function CareerArea(props) {
         <group ref={groupRef} {...props} dispose={null}>
 
             {/* TODO Take the fence segment out of the exclusions list */}
-            <GenericArea nodes={nodes} exclusions={[nodes.fence_segment001, nodes.exhaust_emitter, nodes.rocket, nodes.rocket_nozzle_1, nodes.rocket_nozzle_2, nodes.rocket_cradle]}/>
+            <GenericArea nodes={nodes} exclusions={[nodes.fence_segment001, 
+                                                    nodes.exhaust_emitter, nodes.rocket, nodes.rocket_nozzle_1, nodes.rocket_nozzle_2, nodes.rocket_cradle,
+                                                    nodes.launch_button]}
+            />
 
             <group ref={rocketGroupRef}>
 
@@ -80,8 +109,22 @@ export default function CareerArea(props) {
                 <meshMatcapMaterial matcap={matcapManager.getMatcapByName('rock-gray')} />
             </mesh>
 
-            {/* <RearwardExhaust emitter={nodes.exhaust_emitter} /> */}
+            <RearwardExhaust emitter={nodes.exhaust_emitter} isVisibleRef={exhaustIsVisibleRef} />
             {/* <RocketFlames nozzleRefs={[nozzle1Ref, nozzle2Ref]} /> */}
+
+            {/* Launch button */}
+            <RigidBody type="fixed" onCollisionEnter={() => { launchState.initLaunch = true }}>
+                <mesh ref={launchButtonRef}
+                      geometry={nodes.launch_button.geometry} 
+                      position={nodes.launch_button.position} 
+                      rotation={nodes.launch_button.rotation} 
+                      scale={nodes.launch_button.scale}
+                      onClick={() => {launchState.initLaunch = true}}> {/* Include this click for debugging only */}
+
+                    <meshMatcapMaterial matcap={matcapManager.getMatcapByName('vanguard-red')} />
+                    
+                </mesh>
+            </RigidBody>
             
             {/* Captions */}
             {/* TODO Make these postions dependent on something in the scene */}
