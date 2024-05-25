e71afa412717ce317848689edbf398e3236658b0
Craig Knobaluch <craigknoblauch@gmail.com>
2024-05-25 19:18:44 -0400

    using actual progress on indicator; jumpy

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -1,5 +1,5 @@
 import * as Three from 'three'
-import { shaderMaterial } from "@react-three/drei"
+import { shaderMaterial, useProgress } from "@react-three/drei"
 import loadingIndicatorFragmentShader from '../shaders/loading-indicator/fragment.glsl'
 import loadingIndicatorVertexShader from '../shaders/loading-indicator/vertex.glsl'
 import { useRef } from 'react'
@@ -7,7 +7,7 @@ import { useFrame, extend } from '@react-three/fiber'
 
 const LoadingIndicatorMaterial = shaderMaterial(
     {
-        uProgress: 1.0
+        uProgress: 0.0
     },
     loadingIndicatorVertexShader,
     loadingIndicatorFragmentShader
@@ -19,15 +19,10 @@ export default function LoadingIndicator(props) {
 
     const meshRef = useRef()
     const loadingIndicatorMaterialRef = useRef()
+    let { progress } = useProgress()
 
     useFrame((state, delta) => {
-        // Increment the progress of the loading indicator by 0.01 every frame
-        loadingIndicatorMaterialRef.current.uProgress += 0.01
-
-        // When the progress reaches 1, it will be reset to 0
-        if (loadingIndicatorMaterialRef.current.uProgress >= 1.0) {
-            loadingIndicatorMaterialRef.current.uProgress = 0
-        }
+        loadingIndicatorMaterialRef.current.uProgress = progress/100
     })
     
     return <>
