6a933ce69f1e9fc47b1a4e5917cc932b6ef45f37
Craig Knobaluch <craigknoblauch@gmail.com>
2024-05-26 18:20:12 -0400

    bottom of flames now jump

--- src/shaders/rocket-flames/yellow/vertex.glsl
+++ src/shaders/rocket-flames/yellow/vertex.glsl
@@ -5,8 +5,20 @@ varying vec2 vUv;
 
 void main()
 {
+    vec3 newPosition = position;
 
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    if (uv.y > 0.55) {
+
+        float jumpyPerlin = texture(
+            uPerlinTexture, 
+            vec2(0.5, uv.y * 0.2 - uTime)
+        ).r;
+
+        newPosition.y -= jumpyPerlin * 2.0;
+
+    }
+
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
 
     vUv = uv;
 }
