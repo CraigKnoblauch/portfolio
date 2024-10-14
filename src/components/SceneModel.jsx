0082a79bbb22e4dad14578f657871fa9e981f209
Craig Knoblauch <craigknoblauch@gmail.com>
2024-10-13 18:29:09 -0700

    base component for a model ina scene

---
+++ src/components/SceneModel.jsx
@@ -0,0 +1,28 @@
+import { useRef, useEffect } from 'react'
+import * as THREE from 'three'
+import PropTypes from 'prop-types'
+
+export default function SceneModel( { meshMaterialPairs } ) {
+
+    const group = useRef(new THREE.Group())
+
+    useEffect(() => {
+        const currentGroup = group.current
+        meshMaterialPairs.forEach(([mesh, material]) => {
+            mesh.material = material
+            currentGroup.add(mesh)
+        })
+        return () => {
+            meshMaterialPairs.forEach(([mesh]) => {
+                currentGroup.remove(mesh)
+            })
+        }
+    }, [meshMaterialPairs])
+    
+    return <>
+        <group ref={group} />
+    </>
+}
+SceneModel.propTypes = {
+    meshMaterialPairs: PropTypes.arrayOf(PropTypes.object).isRequired
+};
