2382b28ddcb827e92f6e2bfe6d706c74569a1b02
Craig Knobaluch <craigknoblauch@gmail.com>
2024-11-01 23:30:45 -0400

    mods to front page layout

--- src/components/FrontPage.jsx
+++ src/components/FrontPage.jsx
@@ -1,13 +1,13 @@
 export default function FrontPage() {
   return <>
-    <div className="flex items-center justify-center h-screen bg-customSand">
-      <ul>
-        <li>Craig Knoblauch</li>
-        <li>Experienced Software Engineer</li>
-        <li>Experience a pre-release of my portfolio</li>
-        <li>Best experienced on a desktop</li>
+    <div className="flex items-center justify-center h-screen bg-customSand text-black font-bold">
+      <ul className="flex flex-col items-center justify-center text-black font-bold">
+        <li className="text-8xl">Craig Knoblauch</li>
+        <li className="text-5xl">Experienced Software Engineer</li>
+        <li className="text-2xl">Experience a pre-release of my portfolio</li>
+        <li className="text-lg">Best experienced on a desktop</li>
         <li>
-          <button className="rounded-lg bg-white border border-purple-500 text-purple-500 px-4 py-2">
+          <button className="rounded-lg border-4 border-black px-8 py-2 opacity-50">
             Loading
           </button>
         </li>
