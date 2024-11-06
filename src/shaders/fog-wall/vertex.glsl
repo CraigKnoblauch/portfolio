0a138cdc85f50215dc173d41089b211433abfac7
Craig Knoblauch <craigknoblauch@gmail.com>
2024-11-05 19:58:33 -0800

    clear out all for a rewrite

--- src/shaders/fog-wall/vertex.glsl
+++
@@ -1,9 +0,0 @@
-varying vec2 vUv;
-
-// Shouldn't be a need to change the vertices
-void main()
-{
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-
-    vUv = uv;
-}
--- src/stores/useCameraStore.jsx
+++
@@ -1,6 +0,0 @@
-import { create } from 'zustand'
-
-export const useCameraStore = create((set) => ({
-  cameras: {},
-  addCamera: (camera) => set((state) => ({ cameras: state.cameras[camera.name] = camera }))
-}))
