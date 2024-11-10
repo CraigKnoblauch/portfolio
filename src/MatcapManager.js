ea5eb7c9d096e9b89a8822cf8ac025247f200c09
Craig Knoblauch <craigknoblauch@gmail.com>
2024-11-10 09:39:38 -0800

    attempt to create load matcaps function. Issue in test case I think

--- src/MatcapManager.js
+++ src/MatcapManager.js
@@ -5,13 +5,17 @@ import { TextureLoader } from 'three'
  * Matcaps are flywheels in this application.
  */
 export default class MatcapManager {
-    constructor() {}
+    constructor() {
+        this.matcaps = {}
+        this.textureLoader = new TextureLoader()
+    }
 
     loadMatcaps(matcapFiles) {
         Object.keys(matcapFiles).forEach((imageFilename) => {
             const materialName = imageFilename.split('.')[0]
-            this.matcaps[materialName] = null //useLoader(TextureLoader, ['./matcaps/' + imageFilename]) // Load the matcap
+            this.matcaps[materialName] = this.textureLoader.load('./matcaps/' + imageFilename) // Load the matcap
         })
+        console.log(this.matcaps)
     }
 
     getMatcapByName(name) {
--- tests/MatcapManager.test.js
+++ tests/MatcapManager.test.js
@@ -1,10 +1,24 @@
 import { it, expect, describe } from 'vitest'
 import MatcapManager from '../src/MatcapManager'
+import { TextureLoader } from 'three'
 
 describe('MatcapManager', () => {
     it('Should not be a singleton', () => {
         const mm1 = new MatcapManager()
         const mm2 = new MatcapManager()
         expect(mm1).not.toBe(mm2)
+    })
+
+    it('Should load matcaps', () => {
+        const textureLoader = new TextureLoader()
+        const mm = new MatcapManager()
+        mm.loadMatcaps([
+            'test-material-1.png',
+            'test-material-2.png',
+            'test-material-3.png'
+        ])
+        expect(mm.matcaps['test-material-1']).toBe(textureLoader.load('./matcaps/test-material-1.png'))
+        expect(mm.matcaps['test-material-2']).toBe(textureLoader.load('./matcaps/test-material-2.png'))
+        expect(mm.matcaps['test-material-3']).toBe(textureLoader.load('./matcaps/test-material-3.png'))
     })
 })
