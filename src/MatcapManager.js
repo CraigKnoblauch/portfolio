177543bd9eb3c0b706517a22459b5f4a168c38d3
Craig Knoblauch <craigknoblauch@gmail.com>
2024-11-10 09:19:35 -0800

    get rid of singleton idea. Want to test loading matcaps easier. Will also be injecting matcaps into model components rather than getting a MatcapManager in each one. That means I can define MatcapManager only once

--- src/MatcapManager.js
+++ src/MatcapManager.js
@@ -1,23 +1,14 @@
 import { TextureLoader } from 'three'
-import { matcapImages } from 'src/matcapImages.js'
 
 /**
  * Manages the loading and retrieval of matcaps.
  * Matcaps are flywheels in this application.
  */
 export default class MatcapManager {
-    constructor() {
-        if (!MatcapManager.instance) {
-            this.matcaps = {}
-            this.loadMatcaps()
-            MatcapManager.instance = this
-        }
+    constructor() {}
 
-        return MatcapManager.instance
-    }
-
-    loadMatcaps() {
-        matcapImages.forEach((imageFilename) => {
+    loadMatcaps(matcapFiles) {
+        Object.keys(matcapFiles).forEach((imageFilename) => {
             const materialName = imageFilename.split('.')[0]
             this.matcaps[materialName] = null //useLoader(TextureLoader, ['./matcaps/' + imageFilename]) // Load the matcap
         })
--- tests/MatcapManager.test.js
+++ tests/MatcapManager.test.js
@@ -2,9 +2,9 @@ import { it, expect, describe } from 'vitest'
 import MatcapManager from '../src/MatcapManager'
 
 describe('MatcapManager', () => {
-    it('Should be a singleton', () => {
+    it('Should not be a singleton', () => {
         const mm1 = new MatcapManager()
         const mm2 = new MatcapManager()
-        expect(mm1).toBe(mm2)
+        expect(mm1).not.toBe(mm2)
     })
 })
