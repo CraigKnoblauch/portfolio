46405d75e4abca92a44b978fe18e15e6954cc54d
Craig Knoblauch <craigknoblauch@gmail.com>
2024-11-11 14:15:50 -0800

    work on GenericArea tests; hitting GLTFLoader url issue again

--- GenericArea.js
+++ GenericArea.js
@@ -4,12 +4,20 @@ export default class GenericArea {
     constructor(nodes, matcapMgr, groundMaterial, exclusions) {
         this.matcapMgr = matcapMgr
         this.group = new Group()
+        this.props = new Group()
 
         this.configureProps(nodes, exclusions)
 
         const groundMesh = Object.values(nodes).find(mesh_obj => mesh_obj.isMesh && mesh_obj.name.includes("ground"))
         if (groundMesh) {
-            this.configureGround(groundMesh, groundMaterial)
+            this.ground = new Mesh(groundMesh.geometry, groundMaterial)
+            this.ground.position.set(groundMesh.position)
+            this.ground.rotation.set(groundMesh.rotation)
+        }
+
+        this.group.add(this.props)
+        if (this.ground) { 
+            this.group.add(this.ground) 
         }
     }
 
@@ -18,14 +26,11 @@ export default class GenericArea {
             if (mesh_obj.isMesh && !exclusions.includes(mesh_obj) && !mesh_obj.name.includes("ground")) {
                 const material = this.matcapMgr.getMatcapByName(mesh_obj.material.name)
                 const mesh = new Mesh(mesh_obj.geometry, material)
-                // TODO How do I set the position, rotation, and scale of the mesh
-                this.group.add(mesh)
+                mesh.position.set(mesh_obj.position)
+                mesh.rotation.set(mesh_obj.rotation)
+                this.props.add(mesh)
             }
         })
     }
 
-    configureGround(ground, groundMaterial) {
-        const groundMesh = new Mesh(ground.geometry, groundMaterial)
-        this.group.add(groundMesh)
-    }
 }
