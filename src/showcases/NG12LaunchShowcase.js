0912097e20386f950c22d6e102213ba254531ae5
Craig Knoblauch <craigknoblauch@gmail.com>
2024-11-18 20:35:06 -0800

    work on NG12 showcase

--- src/showcases/NG12LaunchShowcase.js
+++ src/showcases/NG12LaunchShowcase.js
@@ -3,7 +3,12 @@ import Showcase from "./Showcase"
 export default class NG12LaunchShowcase extends Showcase {
     constructor(name, gltf, scene) {
         super(name, gltf, scene)
+        this.setupMeshes(gltf)
+        
 
+    }
+
+    setupMeshes(gltf) {
         let node_names = [
             "exhaust_emitter", "rocket", "rocket_nozzle_1", "rocket_nozzle_2", "rocket_cradle",
             "rocket_yellow_flames_1", "rocket_yellow_flames_2",
@@ -18,6 +23,15 @@ export default class NG12LaunchShowcase extends Showcase {
             }
         })
 
+        // Create rocket animation group. During animations, this group
+        // is updated as one
+        // my brain just turned off
+        this.rocketGroup = new THREE.Group()
+        this.rocketGroup.add(this.nodes["rocket"])
+        this.rocketGroup.add(this.nodes["rocket_nozzle_1"])
+        this.rocketGroup.add(this.nodes["rocket_nozzle_2"])
+        this.rocketGroup.add(this.nodes["rocket_yellow_flames_1"])
+        this.rocketGroup.add(this.nodes["rocket_yellow_flames_2"])
     }
 
     update() {
